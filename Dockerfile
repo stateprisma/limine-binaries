05d0efef5580dc5187338fcfb6b530eddfb56cf2
xadaemon <soft.song@fastmail.net>
2024-09-17 11:46:42 +0200

    feat(build): it works and egenartes a binary for limine

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM docker.io/library/ubuntu:noble
+
+LABEL maintainer="Matheus Xavier <soft.song@fastmail.net"
+
+RUN apt-get update && apt-get upgrade -yy
+
+RUN apt-get install -yy \
+coreutils \
+make \
+sed \
+grep \
+gzip \
+nasm \
+mtools \
+clang \
+curl \
+tar \
+git \
+jq \
+automake \
+llvm
+
+RUN mkdir limine-build
+COPY build.sh /limine-build
+
+VOLUME [ "/out" ]
+
+CMD [ "/limine-build/build.sh" ]
---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# check if a version override was passed in via env
+if [ -n "$LIMINE_VERSION" ]; then
+	tarball_url=$(curl -sL "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/limine-bootloader/limine/releases/$tarball_url" | jq -r '.tarball_url')
+else
+	tarball_url=$(curl -sL "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/limine-bootloader/limine/releases/latest" | jq -r '.tarball_url')
+fi
+
+echo "Using tarball: $tarball_url"
+# download the tarball
+
+curl -SL -o limine.tar.gz "$tarball_url" || exit 255
+# decompress the tarball
+mkdir sources
+tar -xzf limine.tar.gz -C sources --strip-components 1
+
+(
+	export cc=$(which clang)
+
+	cd sources || exit 255
+
+	# Generate the configure script
+	./bootstrap || exit 255
+	mkdir build
+	cd build || exit 255
+	../configure
+	make -j "$(nproc)"
+	# Copy the built files to /out directory
+	tar -cvzf /out/limine.tar.gz .
+	chmod 777 /out/limine.tar.gz
+)
